7a2253b417c613612d7f9e95553c93e7aa8ec4db
strangerintheq <tripmannn@gmail.com>
2019-06-28 00:46:24 +0300

    header encapsulated

--- js/map.js
+++ js/map.js
@@ -1,17 +1,20 @@
-let data = {
-	coords: [60-0.0049, 30.2-0.00273],
-	body: '<img src="logo-white.svg" class="logo2">',
-	footer: 'ул.Мебельная д.47'
-};
+ymaps.ready(function() {
 
-ymaps.ready(() => {
-	let myMap = new ymaps.Map("map", {
-		center: data.coords,
+	var mapData = {
+		coords: [60-0.0049, 30.2-0.00273],
+		body: '<img src="logo-white.svg" class="logo2">',
+		footer: 'ул.Мебельная д.47'
+	};
+
+	var myMap = new ymaps.Map("map", {
+		center: mapData.coords,
 		zoom: 15,
 		controls: []
 	});
+
 	myMap.balloon.open(data.coords, {
-		contentBody: data.body,
-		contentFooter: data.footer
+		contentBody: mapData.body,
+		contentFooter: mapData.footer
 	});
+
 });
